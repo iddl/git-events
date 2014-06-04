65a56c5bf8e1f57973f49b05090bc4e57d74bb92
Ivan Di Lernia <ivan@idilernia.com>
2014-06-03 22:17:42 -0400

    Added X-Poll-Interval checks

--- eventGetter.py
+++ eventGetter.py
@@ -1,4 +1,4 @@
-import requests, re, datetime, json
+import requests, re, datetime, json, datetime, time
 from messages import Messages
 
 GITHUB_EVENT_ENDPOINT = 'https://api.github.com/users/:username/received_events'
@@ -18,8 +18,8 @@ class EventGetterGithub:
         self.username = config["Account"]["username"]
         self.endpoint = re.sub(":username", self.username, GITHUB_EVENT_ENDPOINT)
         self.etag = self.get_etag(self.endpoint)
-        self.last_poll = None
-        self.poll_interval = None
+        self.last_poll = datetime.datetime.utcnow()
+        self.poll_wait = 0
 
     def get_etag(self, endpoint):
         request_headers = {'Authorization' : "token " + self.authorization}
@@ -37,12 +37,19 @@ class EventGetterGithub:
         request_headers['Authorization'] = "token " + self.authorization
         request_headers['If-None-Match'] = self.etag
 
+        time_between_polls = (datetime.datetime.utcnow() - self.last_poll).total_seconds()
+        time_to_wait = self.poll_wait - time_between_polls
+        if time_to_wait > 0:
+            time.sleep(time_to_wait)
+
         request = requests.get(self.endpoint, headers=request_headers)
         self.last_poll = datetime.datetime.utcnow()
 
         if request.status_code == requests.codes.ok:
+            self.poll_wait = int(request.headers["X-Poll-Interval"])
             return request.json()
         elif request.status_code == 304:
+            self.poll_wait = int(request.headers["X-Poll-Interval"])
             return []
         else:
             raise Exception()
